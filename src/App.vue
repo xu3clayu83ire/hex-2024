0e4c29d6b530fb05b7ec27a28caf4851655a8cbd
xu3woli <prostyliu@gmail.com>
2024-07-06 19:45:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+</template>
+
+<style lang="scss">
+@import '~/bootstrap-icons/font/bootstrap-icons.css';
+@import '@/assets/styles/all.scss';
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+      <div>
+        <button type="button" @click="count++">count is {{ count }}</button>
+      </div>
+    </div>
+
+    <div class="my-5">
+      <div class="btn btn-primary">primary</div>
+      <div class="btn btn-secondary">secondary</div>
+      <div class="btn btn-success">success</div>
+      <div class="btn btn-info">info</div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
